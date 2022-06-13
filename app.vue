80af4bf73943e3291306da47a746585390b50569
Melanie de Leeuw <melaniedeleeuw@gmail.com>
2022-06-13 17:27:45 +0200

    Did stuff

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <div id="nav">
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/generate">Generate a bug</NuxtLink>
+    </div>
+    <NuxtPage />
   </div>
 </template>
+
+<style lang="sass">
+@import '~/assets/style/main.scss'
+</style>
---
+++ pages/generate.vue
@@ -0,0 +1,8 @@
+<template>
+  <h2>{{ title }}</h2>
+  <!-- <GenerateBug /> -->
+</template>
+
+<script setup>
+const title = ref('Welcome on the generate page');
+</script>
